55512df74bc3e9b6b93d91846436d2c3a54a9b74
Ayush Srivastava <96059840+AyushSrivastava16@users.noreply.github.com>
2025-05-30 19:10:17 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -25,7 +25,7 @@ function Signup() {
       return handleError("name, email and password are required");
     }
     try {
-      const url = `http://localhost:8080/auth/signup`;
+      const url = `https://major-final-tf45.onrender.com/auth/signup`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
